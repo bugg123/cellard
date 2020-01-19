b9a92e35624eef2c732d8dd0ea93a477bdc6f73c
Corey Hinkle <bugg123@gmail.com>
2020-01-19 16:51:43 -0500

    Refactor search to use json returns

--- beer_search_test.go
+++ beer_search_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,11 +11,33 @@ import (
 
 const searchString = "Bourbon County Brand Stout (2019)"
 
-func TestAddBeerSearchQuery(t *testing.T) {
-	c := NewClient(nil)
+func TestBeerSearchQuery(t *testing.T) {
+	c, done := beerSearchTestClient(t, func(t *testing.T, w http.ResponseWriter, r *http.Request) {
+		if q := r.URL.Query().Get("q"); q != searchString {
+			t.Fatalf("search query doesn't match, got: %q want: %q", q, searchString)
+		}
+
+		var (
+			searchJSON []byte
+			err        error
+		)
+		if r.URL.Query().Get("limit") == "5" {
+			searchJSON, err = ioutil.ReadFile("json/beer_search/beer_search_5.json")
+		} else {
+			searchJSON, err = ioutil.ReadFile("json/beer_search/beer_search.json")
+		}
+		assert.NoErrorf(t, err, "couldn't read JSON file %v", err)
+		w.Write(searchJSON)
+	})
+	defer done()
+	// c := NewClient(nil)
 	t.Run("search simple query", func(t *testing.T) {
 		beers, err := c.Beer.SearchBeerQuery(searchString, 10)
+		assert.NotNil(t, beers)
 		assert.NoError(t, err)
+		if t.Failed() {
+			t.FailNow()
+		}
 
 		//Check default limit is returned
 		assert.Len(t, *beers, 10)
@@ -27,11 +52,28 @@ func TestAddBeerSearchQuery(t *testing.T) {
 	})
 	t.Run("check limit sizes", func(t *testing.T) {
 		fiveBeers, err := c.Beer.SearchBeerQuery(searchString, 5)
+		assert.NotNil(t, fiveBeers)
 		assert.NoError(t, err)
-		sevenBeers, err := c.Beer.SearchBeerQuery(searchString, 7)
-		assert.NoError(t, err)
+		if t.Failed() {
+			t.FailNow()
+		}
 		assert.Len(t, *fiveBeers, 5)
-		assert.Len(t, *sevenBeers, 7)
 	})
+}
 
+func beerSearchTestClient(t *testing.T, fn func(t *testing.T, w http.ResponseWriter, r *http.Request)) (*Client, func()) {
+	return testClient(t, func(t *testing.T, w http.ResponseWriter, r *http.Request) {
+
+		if m := r.Method; m != http.MethodGet {
+			t.Fatalf("expected GET but got unexpected http method: %q", m)
+		}
+
+		prefix := beerSearchPath
+		if p := r.URL.Path; !strings.HasPrefix(p, prefix) {
+			t.Fatalf("expected %q to have prefix %q", p, prefix)
+		}
+		if fn != nil {
+			fn(t, w, r)
+		}
+	})
 }
