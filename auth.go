41302d1a165c8a5495382aecc84a94836d79e3ea
Corey Hinkle <bugg123@gmail.com>
2020-02-03 20:09:39 -0500

    Update auth

--- auth.go
+++ auth.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 const (
@@ -86,4 +88,46 @@ func NewAuthHandler(clientID, clientSecret, redirectURL string, fn TokenHandlerF
 		handler:      fn,
 		client:       client,
 	}, cu, nil
+}
+
+func (a *AuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "only GET requests are allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		http.Error(w, "no 'code' GET parameter", http.StatusBadRequest)
+		return
+	}
+
+	res, err := a.client.Get(a.oAuthURL.String() + "&code=" + code)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer res.Body.Close()
+
+	if c := res.StatusCode; c > 299 || c < 200 {
+		http.Error(w, fmt.Sprintf("authentication server error: HTTP %03d", c), http.StatusBadGateway)
+		return
+	}
+
+	if !strings.Contains(res.Header.Get("Content-Type"), jsonContentType) {
+		http.Error(w, "authentication server sent non-JSON conten", http.StatusBadGateway)
+	}
+
+	var v struct {
+		Response struct {
+			AccessToken string `json:"access_token"`
+		} `json:"response"`
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&v); err != nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
+		return
+	}
+
+	a.handler(v.Response.AccessToken, w, r)
 }
